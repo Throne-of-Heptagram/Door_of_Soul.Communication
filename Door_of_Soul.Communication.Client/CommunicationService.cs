1b57cb2ce87fba2032749733aa62f973f3ff0e94
DSobscure <snbruce13@yahoo.com.tw>
2017-09-08 00:07:24 +0800

    Setup ProxyServer Connection

--- Door_of_Soul.Communication.Client/CommunicationService.cs
+++ Door_of_Soul.Communication.Client/CommunicationService.cs
@@ -21,7 +21,7 @@ namespace Door_of_Soul.Communication.Client
         public bool ProxyServerConnected
         {
             get { return proxyServerConnected; }
-            protected set
+            set
             {
                 proxyServerConnected = value;
                 OnProxyServerConnectStatusChanged?.Invoke(proxyServerConnected);
@@ -37,15 +37,16 @@ namespace Door_of_Soul.Communication.Client
         public abstract bool FindScene(int sceneId, out VirtualScene scene);
         public abstract bool FindSceneServerName(int sceneId, out string sceneServerName);
 
-        protected bool HandleEvent(DeviceEventCode eventCode, Dictionary<byte, object> parameters, out string errorMessage)
+        public bool HandleEvent(DeviceEventCode eventCode, Dictionary<byte, object> parameters, out string errorMessage)
         {
             return DeviceEventRouter.Instance.Route(eventCode, parameters, out errorMessage);
         }
 
-        protected bool HandleOperationResponse(DeviceOperationCode operationCode, OperationReturnCode returnCode, string operationMessage, Dictionary<byte, object> parameters, out string errorMessage)
+        public bool HandleOperationResponse(DeviceOperationCode operationCode, OperationReturnCode returnCode, string operationMessage, Dictionary<byte, object> parameters, out string errorMessage)
         {
             return DeviceOperationResponseRouter.Instance.Route(operationCode, returnCode, operationMessage, parameters, out errorMessage);
         }
+
         public abstract bool ConnectProxyServer(string serverAddress, int port, string applicationName);
         public abstract bool ConnectSceneServer(string serverAddress, int port, string applicationName);
         public abstract void Process();
