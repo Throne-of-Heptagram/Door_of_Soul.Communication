fb8d928213dca9ba7c38501beaca56c022fcff9b
DSobscure <snbruce13@yahoo.com.tw>
2017-09-26 00:59:03 +0800

    add hexagram node type for simpler server setup

---
+++ Door_of_Soul.Communication.HexagramEntranceServer/HexagramNodeServerType.cs
@@ -0,0 +1,18 @@
+﻿namespace Door_of_Soul.Communication.HexagramEntranceServer
+{
+    public enum HexagramNodeServerType : byte
+    {
+        Knowledge,
+        Life,
+        Element,
+        Infinite,
+        Love,
+        Space,
+        Will,
+        Shadow,
+        History,
+        Eternity,
+        Destiny,
+        Throne
+    }
+}
